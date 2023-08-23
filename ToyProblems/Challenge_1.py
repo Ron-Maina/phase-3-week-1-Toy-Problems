5553922760ae6bacad6c5ab4bbd02af9105df72d
Ron Maina <ronmwangi00@gmail.com>
2023-08-23 14:24:44 +0300

    Updated time converter

--- ToyProblems/Challenge_1.py
+++ ToyProblems/Challenge_1.py
@@ -3,15 +3,15 @@ def time_converter(hour, min, period):
     if period == "am" and min < 60:
         if hour == 12:
             hour = 0
-            print(f"{hour :02d}{min} hrs")
+            print(f"{hour :02d}{min :02d} hrs")
         elif 1 <= hour <= 11:
-            print(f"{hour :02d}{min} hrs")
+            print(f"{hour :02d}{min :02d} hrs")
     
     elif period == "pm" and min < 60:
         if hour == 12:
-            print(f"{hour}{min} hrs")
+            print(f"{hour}{min :02d} hrs")
         elif 1 <= hour <= 11:
-            print(f"{hour + 12}{min} hrs")
+            print(f"{hour + 12}{min :02d} hrs")
             
     else:
         print("Invalid Time format")
